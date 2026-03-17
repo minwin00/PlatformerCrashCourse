e9d1dcec8437093b5c0983baee5e609b70c81352
Yeon <rst0272@naver.com>
2026-03-17 21:50:40 +0900

    lec17 Enemy Attack Cooldown & Cliff Detection

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -8,6 +8,7 @@ public class Knight : MonoBehaviour
     public float walkSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
     Animator animator;
     Damagable damagable;
 
@@ -63,6 +64,12 @@ public class Knight : MonoBehaviour
         get { return animator.GetBool(AnimationStrings.canMove); }
     }
 
+    public float AttackCooldown
+    {
+        get { return animator.GetFloat(AnimationStrings.attackCooldown); }
+        set { animator.SetFloat(AnimationStrings.attackCooldown, Mathf.Max(0, value)); }
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -74,6 +81,10 @@ public class Knight : MonoBehaviour
     void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
+        if (AttackCooldown > 0)
+        {
+            AttackCooldown -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate()
@@ -116,5 +127,13 @@ public class Knight : MonoBehaviour
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.linearVelocity = new Vector2(knockback.x, rb.linearVelocity.y + knockback.y);
+    }
+
+    public void OnCliffDetected()
+    {
+        if (touchingDirections.IsGrounded)
+        {
+            FlipDirection();
+        }
     }
 }
