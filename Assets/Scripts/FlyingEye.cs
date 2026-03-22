635807d6598f8a686d72f7a8093463f34e1a7e00
Yeon <rst0272@naver.com>
2026-03-22 19:35:27 +0900

    lec22 FlyingEye Waypoint

--- Assets/Scripts/FlyingEye.cs
+++ Assets/Scripts/FlyingEye.cs
@@ -1,10 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FlyingEye : MonoBehaviour
 {
+    public float flightSpeed = 2f;
+    public float waypointReachedDistance = 0.1f;
     public DetectionZone biteDetectionZone;
+    public List<Transform> waypoints = new List<Transform>();
+
     Animator animator;
     Rigidbody2D rb;
+    Damageable damageable;
+
+    Transform nextWaypoint;
+    int waypointNum = 0;
 
     public bool _hasTarget = false;
     public bool HasTarget
@@ -17,15 +27,57 @@ public class FlyingEye : MonoBehaviour
         }
     }
 
+    public bool CanMove
+    {
+        get { return animator.GetBool(AnimationStrings.canMove); }
+    }
+
     void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        damageable = GetComponent<Damageable>();
     }
 
     // Update is called once per frame
     void Update()
     {
         HasTarget = biteDetectionZone.detectedColliders.Count > 0;
+    }
+
+    private void Start()
+    {
+        nextWaypoint = waypoints[waypointNum];
+    }
+
+    private void FixedUpdate()
+    {
+        if (damageable.IsAlive)
+        {
+            if (CanMove)
+            {
+                Flight();
+            }
+            else
+            {
+                rb.linearVelocity = Vector3.zero;
+            }
+        }
+    }
+
+    private void Flight()
+    {
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+        float distance = Vector2.Distance(transform.position, nextWaypoint.position);
+        rb.linearVelocity = directionToWaypoint * flightSpeed;
+        if (distance <= waypointReachedDistance)
+        {
+            waypointNum++;
+            if (waypointNum >= waypoints.Count)
+            {
+                waypointNum = 0;
+            }
+            nextWaypoint = waypoints[waypointNum];
+        }
     }
 }
